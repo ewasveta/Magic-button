12e2e1c12c29ae44ae76d84e059843cdb641ee4d
EwaSveta <ewasveta@gmail.com>
2023-02-08 22:10:32 +0200

    updated counter of double

--- dice_game/dices/source.js
+++ dice_game/dices/source.js
@@ -5,4 +5,19 @@ function magic()
 
     document.getElementById("dice1").src = num1 + ".jpg";
     document.getElementById("dice2").src = num2 + ".jpg";
+
+    console.log(parseInt(document.getElementById('counter').innerText.split(":")[1]));
+
+    if(num1 == num2)
+    {
+        let value = 
+        parseInt(document.getElementById('counter').innerText.split(":")[1]); 
+            value = isNaN(value) ? 0 : value;
+            
+        value++; 
+
+        document.getElementById('counter').innerText =
+            "כמות הפעמים שיצא לך דאבל : " + value;    
+       
+    }
 }
